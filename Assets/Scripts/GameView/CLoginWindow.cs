ecc8afed560a93e47aaa1f1d4957ca3a1ac56845
dc <dc1539389270@aliyun.com>
2017-02-15 15:26:05 +0800

    提交MVC的框架基本目录

---
+++ Assets/Scripts/GameView/CLoginWindow.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CLoginWindow : CBaseWindow
+{
+
+    public CLoginWindow()
+    {
+
+    }
+    ////////////////////////////继承接口/////////////////////////
+    //类对象初始化
+    public override void Init()
+    {
+       
+    }
+
+
+    //类对象释放
+    public override void Realse()
+    {
+      
+    }
+
+    //窗口控件初始化
+    protected override void InitWidget()
+    {
+       
+    }
+
+
+
+    protected override void RealseWidget()
+    {
+
+    }
+
+
+    public override void OnEnable()
+    {
+
+    }
+    //隐藏
+    public override void OnDisable()
+    {
+
+    }
+
+    //游戏事件注册
+    protected override void OnAddListener()
+    {
+
+    }
+
+    //游戏事件注消
+    protected override void OnRemoveListener()
+    {
+
+    }
+   
+
+}
---
+++ Assets/Scripts/GameView/WindowsManager.cs
@@ -0,0 +1,136 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public enum EScenesType
+{
+    EST_None,
+    EST_Login,
+    EST_Play,
+}
+
+public enum EWindowType
+{
+    EWT_LoginWindow,
+}
+
+public class WindowsManager : Singleton<WindowsManager> {
+
+    private Dictionary<EWindowType, CBaseWindow> m_WindowDic;
+	public WindowsManager()
+    {
+        m_WindowDic = new Dictionary<EWindowType, CBaseWindow>();
+
+        m_WindowDic[EWindowType.EWT_LoginWindow] = new CLoginWindow();
+    }
+
+    public CBaseWindow GetWindow(EWindowType type)
+    {
+        if (m_WindowDic.ContainsKey(type))
+            return m_WindowDic[type];
+
+        return null;
+    }
+
+    public void Update(float deltaTime)
+    {
+        foreach (CBaseWindow pWindow in m_WindowDic.Values)
+        {
+            if (pWindow.IsVisible())
+            {
+                pWindow.Update(deltaTime);
+            }
+        }
+    }
+
+    public void ChangeScenseToPlay(EScenesType front)
+    {
+        foreach (CBaseWindow pWindow in m_WindowDic.Values)
+        {
+            if (pWindow.GetScenseType() == EScenesType.EST_Play)
+            {
+                pWindow.Init();
+
+                if (pWindow.IsResident())
+                {
+                    pWindow.PreLoad();
+                }
+            }
+            else if ((pWindow.GetScenseType() == EScenesType.EST_Login) && (front == EScenesType.EST_Login))
+            {
+                pWindow.Hide();
+                pWindow.Realse();
+
+                if (pWindow.IsResident())
+                {
+                    pWindow.DelayDestory();
+                }
+            }
+        }
+    }
+
+    public void ChangeScenseToLogin(EScenesType front)
+    {
+        foreach (CBaseWindow pWindow in m_WindowDic.Values)
+        {
+            if (front == EScenesType.EST_None && pWindow.GetScenseType() == EScenesType.EST_None)
+            {
+                pWindow.Init();
+
+                if (pWindow.IsResident())
+                {
+                    pWindow.PreLoad();
+                }
+            }
+
+            if (pWindow.GetScenseType() == EScenesType.EST_Login)
+            {
+                pWindow.Init();
+
+                if (pWindow.IsResident())
+                {
+                    pWindow.PreLoad();
+                }
+            }
+            else if ((pWindow.GetScenseType() == EScenesType.EST_Play) && (front == EScenesType.EST_Play))
+            {
+                pWindow.Hide();
+                pWindow.Realse();
+
+                if (pWindow.IsResident())
+                {
+                    pWindow.DelayDestory();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// 隐藏该类型的所有Window
+    /// </summary>
+    /// <param name="front"></param>
+    public void HideAllWindow(EScenesType front)
+    {
+        foreach (var item in m_WindowDic)
+        {
+            if (front == item.Value.GetScenseType())
+            {
+                Debug.Log(item.Key);
+                item.Value.Hide();
+                //item.Value.Realse();
+            }
+        }
+    }
+
+    public void ShowWindowOfType(EWindowType type)
+    {
+        CBaseWindow window;
+        if (!m_WindowDic.TryGetValue(type, out window))
+        {
+            return;
+        }
+        window.Show();
+    }
+
+}
